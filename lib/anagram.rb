8d31bce12d80d5edf9ca7017ffc35af678ac3cea
deanban <pbp2005@gmail.com>
2017-07-23 13:27:47 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+
+	attr_accessor :word 
+
+	def initialize word
+		@word = word
+	end
+
+	def match word_arr
+		word_arr.select do |words|
+			words.split("").sort == self.word.split("").sort
+		end
+	end
+end
